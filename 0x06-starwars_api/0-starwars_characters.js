b75fa6be39146d5a67552a2af0c876fbd0267e8b
Droid <effahfrancis210@gmail.com>
2024-09-05 17:44:01 +0000

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+function fetchData (url) {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (err, res, body) => {
+      if (err) reject(err);
+      else resolve(body);
+    });
+  });
+}
+
+function fetchCharacter (characterUrl) {
+  return fetchData(characterUrl).then(body => body.name);
+}
+
+fetchData(url)
+  .then(movie => {
+    const characterUrls = movie.characters;
+    return Promise.all(characterUrls.map(fetchCharacter));
+  })
+  .then(names => names.forEach(name => console.log(name)))
+  .catch(err => console.error('Error:', err));
